38767186fc68f005a25c5c9a95c434e25d861cd2
nikhil-ns <nikhiladi19@gmail.com>
2024-02-01 12:12:42 +0530

    inital Files

--- 01/01_node.mjs
+++ 01/01_node.mjs
@@ -3,6 +3,7 @@ Problem 1: File Reader
 Problem Statement: Create a function readFileContent(filePath) that takes
 the path to a file as input and reads its content asynchronously using the fs module. 
 The function should print the content to the console.
+
 */
 
 import {readFile, readFileSync} from 'fs';
