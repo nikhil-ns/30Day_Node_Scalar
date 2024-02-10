4cfeb8ee3cef3eb58ada1a8972ebd16064c21805
nikhil-ns <nikhiladi19@gmail.com>
2024-02-10 18:54:08 +0530

    day 10

---
+++ 09/09_node.js
@@ -0,0 +1,16 @@
+/*
+
+Day 09 of 30 days of NodeJs
+
+Which of the following are examples of node modules?
+
+a. Express
+
+b. Body-parser
+
+c. Socket.io
+
+d. All of the above
+
+Ans : d) All of the above
+*/
